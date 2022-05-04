8aa96644052ae677261d67139534f7ff30b6a76e
Dimario49 <chubarovdima@inbox.ru>
2022-05-04 22:21:35 +0300

    Переделал TimeText

--- src/TimeText.jsx
+++ src/TimeText.jsx
@@ -1,36 +1,80 @@
 import * as React from "react";
-import {Typography} from "@mui/material";
-import Box from '@mui/material/Box';
+import { TextField, Typography, Box } from "@mui/material";
 
 export default function TimeText({
   realHour = 0,
-  targetHour = 0,
+  targetHourState,
   TimeType = "astronomical"
 }) {
 
-  if (TimeType === "academic")
-  {
-    realHour = (realHour * 60) / 45 
+  if (TimeType === "academic") {
+    realHour = (realHour * 60) / 45
     realHour = realHour.toFixed(1)
-    targetHour = (targetHour * 60) / 45 
-    targetHour = targetHour.toFixed(1)
   }
 
-  return (
-    <Box sx={{display: "flex", gap: 2 }}>
+  const [isTimeFieldOpen, setIsTimeFieldOpen] = React.useState(false);
+  const [targetHour, setTargetHour] = React.useState("0");
+  const timeRef = React.useRef("")
+
+  React.useEffect(() => {
+    const closeTimeField = (e) => {
+      if (!timeRef.current.contains(e.target)) {
+        setTargetHour(timeRef.current.value);
+        setIsTimeFieldOpen(false);
+        if (targetHourState !== undefined) {
+          targetHourState(targetHour);
+        }
+      }
+    };
+    document.addEventListener("click", closeTimeField);
+    return () => document.removeEventListener("click", closeTimeField);
+  }, []);
+
+  if (isTimeFieldOpen) {
+    return (
+      <Box sx={{ display: "flex", gap: 2 }}>
         <Typography variant="body2" color='gray'>
           {realHour}
         </Typography>
-        <Typography variant="body2"color='gray'>
+        <Typography variant="body2" color='gray'>
           /
         </Typography>
-        <Typography variant="body2">
+        <TextField
+          sx={{ width: "36px" }}
+          defaultValue={targetHour}
+          inputRef={timeRef}
+          type="number"
+          size="small"
+          variant="standard"
+          onKeyPress={(e) => {
+            if (e.key === "Enter") {
+              setTargetHour(timeRef.current.value);
+              setIsTimeFieldOpen(false);
+            }
+          }}
+        />
+        <Typography variant="body2" color='gray'>
+          ч.
+        </Typography>
+      </Box>
+    );
+  }
+  else {
+    return (
+      <Box sx={{ display: "flex", gap: 2 }}>
+        <Typography variant="body2" color='gray'>
+          {realHour}
+        </Typography>
+        <Typography variant="body2" color='gray'>
+          /
+        </Typography>
+        <Typography variant="body2" onDoubleClick={() => setIsTimeFieldOpen(true)}>
           {targetHour}
         </Typography>
         <Typography variant="body2" color='gray'>
           ч.
         </Typography>
-    </Box>
+      </Box>
     );
   }
-  
+}
